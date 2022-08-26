18f6a8829bf28e97b101a4bdc95f47bed75834f5
Ali Akbar <59540270+LoliC0d3@users.noreply.github.com>
2022-08-26 21:38:24 +0700

    SubHunt

---
+++ subhunt.py
@@ -0,0 +1,14 @@
+import sys
+import urllib.request
+import urllib.parse
+import re
+# python3 subdo.py site.com
+for i, arg in enumerate(sys.argv, 1):
+	domains = set()
+	with urllib.request.urlopen('https://crt.sh/?q=' + urllib.parse.quote('%.' + arg)) as r:
+		code = r.read().decode('utf-8')
+		for cert, domain in re.findall('<tr>(?:\s|\S)*?href="\?id=([0-9]+?)"(?:\s|\S)*?<td>([*_a-zA-Z0-9.-]+?\.' + re.escape(arg) + ')</td>(?:\s|\S)*?</tr>', code, re.IGNORECASE):
+			domain = domain.split('@')[-1]
+			if not domain in domains:
+				domains.add(domain)
+				print(domain)
